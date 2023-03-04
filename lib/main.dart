13a38c92a0f03bcbdbf92cbb55ca308d54e34e45
unique boy <information11993@gmail.com>
2023-03-04 23:59:35 +0545

    login in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
-import 'package:flutter/material.dart';
-import "package:font_awesome_flutter/font_awesome_flutter.dart";
+import 'dart:async';
 
-void main() { 
+import 'package:dart/home.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
@@ -13,163 +17,276 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var weight_controller = TextEditingController();
-  var height_controller = TextEditingController();
-  var color = Colors.yellow;
-  var bmi_value = "";
-  dynamic bmi;
-  var result = "";
+  bool show_hide_password = true;
+  var username_controller = TextEditingController();
+  var password_controller = TextEditingController();
+  var opacity = 0.0;
+  var error_opacity = 0.0;
   var error_message = "";
+  var successful_message = "";
+  var icon = FontAwesomeIcons.eye;
+
+  @override
+  void initState() {
+    super.initState();
+    decide();
+  }
+
+// when user is already logged in then navigating user to home screen
+  decide() async{
+    var pref = await SharedPreferences.getInstance();
+    var is_login = pref.getString("login_successful");
+
+    if(is_login == "true"){
+      Navigator.push(context,
+      MaterialPageRoute(
+        builder: (context) => HomePage(),
+        
+        ));
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          centerTitle: true,
-          title: Text("BMI calculator"),
+          title: const Text("Home Screen"),
         ),
-        body: Container(
-          color: color,
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: SizedBox(
-                width: 300,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Opacity(
+              opacity: error_opacity,
+              child: Container(
+                width: 310,
+                color: Colors.red,
+                child: Row(
                   children: [
-                    TextField(
-
-                      controller: weight_controller,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        hintText: "Enter weight",
-                        suffixText: "KG",
-                        hintStyle: TextStyle(
-                          fontSize: 18,
-                          color: Colors.red,
-                          fontStyle: FontStyle.italic
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)
-                        )
-                      ),
-              
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextField(
-                      controller: height_controller,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        hintText: "Enter height",
-                        suffixText: "Feet.Inch",
-                        hintStyle: TextStyle(
-                          fontSize: 18,
-                          color: Colors.red,
-                          fontStyle: FontStyle.italic
-                        ),
-                        border: OutlineInputBorder(
-                           borderRadius: BorderRadius.circular(20)
-                        )
-                      ),
-              
-                    ),
                     Padding(
-                      padding: const EdgeInsets.only(top:10),
-                      child: SizedBox(
-                        width: 180,
-                        height: 40,
-                        child: ElevatedButton(
-                          onPressed: (){
-                            
-                            if(weight_controller.text != "" && height_controller.text != ""){
-                              error_message = "";
-                              var weight = double.parse(weight_controller.text);
-                            dynamic height = height_controller.text;
-                            height = height.split(".");
-                            var feet = int.parse(height[0]);
-                            var inch = 0;
-                            if(height.length != 1){
-                            inch = int.parse(height[1]);
-        
-                            }
-                            var meter = double.parse(((feet * 30.48 + inch * 2.54) / 100).toStringAsFixed(4));
-                            bmi = weight / (meter * meter);
-                            bmi = double.parse(bmi.toStringAsFixed(1));
-                            bmi_value = "Your BMI is $bmi";
-                            // normal : bmi from 18.5 to 25
-                            // underweight : bmi below 18.5
-                            // overweight : bmi from 25 to 30
-                            // obesity : bmi greater than 30
-        
-                            if(bmi < 18.5){
-                              result = "Underweight";
-                              color = Colors.blue;
-                            }
-                            else if(bmi >= 18.5 && bmi <= 25){
-                              result = "Normal";
-                              color = Colors.green;
-                            }
-                            else if(bmi > 25 && bmi <= 30){
-                              result = "Overweight";
-                              color = Colors.pink;
-                            }
-                            else{
-                              result = "Obesity";
-                              color = Colors.red;
-                            }
-                            result = "You are $result";
-
-                            }
-                            else{
-                              error_message = "Please Fill all the fields";
-                              bmi_value = "";
-                              result = "";
-                              color = Colors.yellow;
-                            }
-                            
-                           
-                            setState(() {
-                              
-                            });
-        
-        
-                          }, 
-                          child: Text("Calculate BMI", style: TextStyle(
-                            fontSize: 20
-                          ),)
-                          
-                          ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        error_message,
+                        style: TextStyle(fontSize: 18, color: Colors.white),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top:15.0),
-                      child: Text("$bmi_value",  style: TextStyle(
-                        fontSize: 20
-                      ),),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top:15.0),
-                      child: Text("$result",  style: TextStyle(
-                        fontSize: 20
-                      ),),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top:15.0),
-                      child: Text("$error_message",  style: TextStyle(
-                        fontSize: 20
-                      ),),
-                    )
                   ],
                 ),
               ),
             ),
-          ),
-        )
-        
-        
-        
-    );
+            AnimatedOpacity(
+              duration: Duration(seconds: 1),
+              opacity: opacity,
+              child: Container(
+                width: 310,
+                color: Colors.green,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        successful_message,
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          opacity = 0;
+                          setState(() {});
+                        },
+                        icon: FaIcon(
+                          FontAwesomeIcons.xmark,
+                          color: Colors.white,
+                          size: 30,
+                        ))
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: [
+                Center(
+                  child: SizedBox(
+                    width: 300,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: TextField(
+                            controller: username_controller,
+                            style: const TextStyle(fontSize: 23),
+                            decoration: const InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.blue)),
+                              label: Text("Enter Username"),
+                              labelStyle: TextStyle(fontSize: 17),
+                              prefixIcon: Icon(Icons.person_2_rounded),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: TextField(
+                            controller: password_controller,
+                            style: const TextStyle(fontSize: 23),
+                            obscureText: show_hide_password,
+                            obscuringCharacter: "*",
+                            decoration: InputDecoration(
+                              focusedBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.blue)),
+                              label: const Text("Enter Password"),
+                              labelStyle: const TextStyle(fontSize: 17),
+                              prefixIcon: const Icon(Icons.password),
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  if (show_hide_password) {
+                                    show_hide_password = false;
+                                    icon = FontAwesomeIcons.eyeSlash;
+                                  } else {
+                                    show_hide_password = true;
+                                    icon = FontAwesomeIcons.eye;
+                                  }
+
+                                  setState(() {});
+                                },
+                                icon: FaIcon(
+                                  icon,
+                                  size: 30,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: 100,
+                                height: 50,
+                                child: ElevatedButton(
+                                    onPressed: () async {
+                                      var username = username_controller.text;
+                                      var password = password_controller.text;
+                                      if (username == "" && password == "") {
+                                        error_opacity = 1;
+                                        error_message =
+                                            "Username and Password are emtpy";
+                                        setState(() {});
+                                        return;
+                                      } else if (username == "") {
+                                        error_opacity = 1;
+                                        error_message = "Username is empty";
+                                        setState(() {});
+                                        return;
+                                      } else if (password == "") {
+                                        error_opacity = 1;
+                                        error_message = "Password is empty";
+                                        setState(() {});
+                                        return;
+                                      }
+                                      var pref =
+                                          await SharedPreferences.getInstance();
+                                      var s_username =
+                                          pref.getString("username");
+                                      var s_password =
+                                          pref.getString("password");
+
+                                      if (username == s_username &&
+                                          password == s_password) {
+                                            error_opacity = 0;
+                                            pref.setString("login_successful", "true");
+                                            print(pref.getString("login_successful"));
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => HomePage(),
+                                            ));
+                                      } else {
+                                        error_message = "Wrong Credentials";
+                                        error_opacity = 1;
+                                        setState(() {});
+                                      }
+                                    },
+                                    child: const Text("Login",
+                                        style: TextStyle(fontSize: 20))),
+                              ),
+                              SizedBox(
+                                width: 50,
+                              ),
+                              SizedBox(
+                                width: 100,
+                                height: 50,
+                                child: ElevatedButton(
+                                    onPressed: () async {
+                                      var pref =
+                                          await SharedPreferences.getInstance();
+                                      pref.remove("username");
+                                      pref.remove("password");
+                                      opacity = 1;
+                                      successful_message =
+                                          "Successfully Logout";
+                                        pref.setString("login_successful", "false");
+                                      Timer(Duration(seconds: 3), () {
+                                        opacity = 0;
+                                        setState(() {});
+                                      });
+                                      setState(() {});
+                                    },
+                                    child: const Text("Logout",
+                                        style: TextStyle(fontSize: 18))),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 100,
+                          height: 50,
+                          child: ElevatedButton(
+                              onPressed: () async {
+                                var username = username_controller.text;
+                                var password = password_controller.text;
+                                if (username == "" && password == "") {
+                                  error_opacity = 1;
+                                  error_message =
+                                      "Username and Password are emtpy";
+                                  setState(() {});
+                                } else if (username == "") {
+                                  error_opacity = 1;
+                                  error_message = "Username is empty";
+                                  setState(() {});
+                                } else if (password == "") {
+                                  error_opacity = 1;
+                                  error_message = "Password is empty";
+                                  setState(() {});
+                                }
+                                if (username != "" && password != "") {
+                                  error_opacity = 0;
+                                  var pref =
+                                      await SharedPreferences.getInstance();
+                                  pref.setString("username", username);
+                                  pref.setString("password", password);
+                                  successful_message =
+                                      "Account Created Successfully";
+                                  opacity = 1;
+                                  Timer(Duration(seconds: 3), () {
+                                    opacity = 0;
+                                    setState(() {});
+                                  });
+                                  setState(() {});
+                                }
+                              },
+                              child: const Text("SignUp",
+                                  style: TextStyle(fontSize: 18))),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ));
   }
 }
